aca56eb40e2f6de292238ea82ac422c0984b4e6a
Rafael <rafaasigoli1@gmail.com>
2025-03-28 16:40:50 -0300

    atualizando

--- src/br/com/restaurante/estacionamento/models/Estacionamento.java
+++ src/br/com/restaurante/estacionamento/models/Estacionamento.java
@@ -6,8 +6,8 @@ import java.sql.*;
 
 public class Estacionamento {
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-    private static final String USER = "rm554019";
-    private static final String PASSWORD = "060304";
+    private static final String USER = "COLOCAR USUARIO";
+    private static final String PASSWORD = "COLOCAR SENHA";
 
     public void mostrarEstacionamento() {
         String query = "SELECT total_vagas, vagas_ocupadas, preco_por_hora FROM estacionamento WHERE id = 1";
